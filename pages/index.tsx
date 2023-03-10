cf8917ad4b6997f15949258b7733b37ecb3c4c82
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-03-10 11:40:32 +0000

    added global styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 
-import { PostCard, Categories, PostWidget } from './components';
+import { PostCard, Categories, PostWidget } from '../components';
 
 const posts = [
   {
