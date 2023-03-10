26b3786044083e38ac7c44e88ba4fa41a25a74cc
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-03-10 13:34:12 +0000

    added post image to post code

--- services/index.js
+++ services/index.js
@@ -4,36 +4,34 @@ const graphqlAPI = process.env.NEXT_PUBLIC_CMS_ENDPOINT
 
 export const getPosts = async () => {
   const query = gql`
-query Assets {
-  assets {
-    createdAt
-    id
-    publishedAt
-    fileName
-    url
-    updatedAt
-  }
-  postsConnection {
-    edges {
-      node {
-        author {
-          bio
-          name
-          id
-        }
-        createdAt
-        slug
-        title
-        excert
-        categories {
-          name
+  query Assets {
+    postsConnection {
+      edges {
+        node {
+          author {
+            bio
+            name
+            id
+            photo {
+              url
+            }
+          }
+          createdAt
           slug
+          title
+          excert
+          featuredImage {
+            url
+          }
+          categories {
+            name
+            slug
+          }
         }
       }
     }
   }
-}
-`
+  `;
 
   const result = await request(graphqlAPI, query)
 
