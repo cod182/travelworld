d92f2cb087fa13a037736748eb4af4c5c7977794
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-03-10 14:18:38 +0000

    added services for similar posts

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import Link from 'next/link';
 
-const PostWidget = ({}) => {
+import { getRecentPosts } from '../services';
+
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(category, slug).then((res) => setRelatedPosts(res));
+    } else {
+      getRecentPosts().then((res) => setRelatedPosts(res));
+    }
+  }, []);
   return <div>PostWidget</div>;
 };
 
