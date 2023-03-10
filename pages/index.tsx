efda28a9adce8a8452020967144faaf9c70ed9b7
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-03-10 11:34:48 +0000

    added posts and categoeis to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 
+import { PostCard, Categories, PostWidget } from './components';
+
 const posts = [
   {
     title: 'Testing',
@@ -9,6 +11,14 @@ const posts = [
   },
   {
     title: 'Testing No.2',
+    excert: 'testing testing twice',
+  },
+  {
+    title: 'Testing No.3',
+    excert: 'testing testing twice',
+  },
+  {
+    title: 'Testing No.4',
     excert: 'testing testing twice',
   },
 ];
@@ -21,14 +31,19 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid-cols-1 lg:grid-cols-12 gap-12">
-        {[
-          posts.map((post, index) => (
-            <div>
-              {post.title}
-              {post.title}
-            </div>
-          )),
-        ]}
+        <div className="lg:col-span-8 col-span-1">
+          {[
+            posts.map((post, index) => (
+              <PostCard post={post} key={post.title} />
+            )),
+          ]}
+        </div>
+        <div className="lg:span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
       </div>
     </div>
   );
