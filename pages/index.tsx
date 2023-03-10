87f106eae6fc5636464b87d5503564777ba27408
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-03-10 13:12:15 +0000

    added api call for posts to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,41 +4,20 @@ import Image from 'next/image';
 
 import { PostCard, Categories, PostWidget } from '../components';
 
-const posts = [
-  {
-    title: 'Testing',
-    excert: 'testing testing',
-  },
-  {
-    title: 'Testing No.2',
-    excert: 'testing testing twice',
-  },
-  {
-    title: 'Testing No.3',
-    excert: 'testing testing twice',
-  },
-  {
-    title: 'Testing No.4',
-    excert: 'testing testing twice',
-  },
-];
+import { getPosts } from '../services';
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>TravelWorld</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="grid-cols-1 lg:grid-cols-12 gap-12">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {[
-            posts.map((post, index) => (
-              <PostCard post={post} key={post.title} />
-            )),
-          ]}
+          {[posts.map((post) => <PostCard post={post} key={post.title} />)]}
         </div>
-        <div className="lg:span-4 col-span-1">
+        <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
             <PostWidget />
             <Categories />
@@ -50,3 +29,10 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
+}
