7fe6d853ceceea565d1d60f6341f6b9f0a771b8b
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-03-10 19:38:39 +0000

    added featured posts and featured post card

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,8 +14,8 @@ const Home: NextPage = ({ posts }) => {
         <title>TravelWorld</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPost />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <FeaturedPost />
         <div className="lg:col-span-8 col-span-1">
           {[
             posts.map((post) => <PostCard post={post.node} key={post.title} />),
