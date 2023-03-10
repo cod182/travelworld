d76797d4ff542740e7bf90b32757edef72a21885
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-03-10 18:20:32 +0000

    added query for comments

--- services/index.js
+++ services/index.js
@@ -141,3 +141,18 @@ export const submitComment = async (obj) => {
 
   return result.json();
 };
+
+export const getComments = async (slug) => {
+  const query = gql`
+    query GetComments(slug:string!) {
+      comments(where: {post: {slug: $slug}}) {
+        name
+        createdAt
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.comments;
+};
