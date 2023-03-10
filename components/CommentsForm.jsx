77e6dc4e41198589950845f1f98d0c3d81467828
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-03-10 16:24:41 +0000

    added input for form

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -10,7 +10,45 @@ const CommentsForm = ({ slug }) => {
   const emailEl = useRef();
   const storeDataEl = useRef();
 
-  return <div>CommentsForm</div>;
+  const handleCommentSubmission = () => {
+    
+  };
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        Comments Form
+      </h3>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <textarea
+          ref={commentEl}
+          className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Comment"
+          name="comment"
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-4 mb-4 lg:grid-cols-2">
+        <input
+          type="text"
+          ref={nameEl}
+          className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          type="text"
+          ref={emailEl}
+          className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Email"
+          name="email"
+        />
+      </div>
+      {error && <p className="text-xs text-red-500">All Fields Are Required</p>}
+      <div className="mt-8">
+        <button type="button" onClick={handleCommentSubmission}></button>
+      </div>
+    </div>
+  );
 };
 
 export default CommentsForm;
